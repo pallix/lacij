8e034f16b8cecbcc8a449a2212483e78294f688f
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-05-04 10:37:07 +0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lacij "0.1.0"
+(defproject lacij "0.1.1"
   :description "Lacij is a graph visualization library for Clojure
                 with automatic layouts, edition and undo/redo support."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [tikkba "0.1.0-SNAPSHOT"]]
+                 [tikkba "0.1.1-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   ;; :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   )
