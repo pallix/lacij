1a9d9567c91e3b778c0cbf97016f1de6bdcb6436
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-06-15 15:56:08 +0200

    updated tikkba version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Lacij is a graph visualization library for Clojure
                 with automatic layouts, edition and undo/redo support."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [tikkba "0.1.1-SNAPSHOT"]]
+                 [tikkba "0.2.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   ;; :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   )
