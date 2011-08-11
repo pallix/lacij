e970e154530f12f5c39007aa4c172b1321745304
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-08-11 11:24:47 +0200

    replaced -SNAPSHOT dependencies by fixed dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lacij "0.4.0-SNAPSHOT"
   :description "Lacij is a graph visualization library for Clojure
                 with automatic layouts, edition and undo/redo support."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [tikkba "0.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [tikkba "0.2.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   ;; :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   )
