de8871d0ca7b7b3cc0ae27811c467b6ae4d086bc
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-06-18 11:32:31 +0200

    allow the use of clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lacij "0.7.0-SNAPSHOT"
   :description "Lacij is a graph visualization library for Clojure
                 with automatic layouts, edition and undo/redo support."
-  :dependencies [[org.clojure/clojure "[1.2.1,1.3.0]"]
+  :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
                  [tikkba "0.3.0"]])
 
