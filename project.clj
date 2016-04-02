cf8e752dc83da32757591764ca688acf84511fac
Pierre Allix <pierre.allix@liqd.de>
2016-04-02 20:11:53 +0200

    Set license and project URL in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject lacij/lacij "0.9.1"
   :min-lein-version "2.0.0"
+  :url "https://github.com/pallix/lacij"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [tikkba "0.6.0"]]
   :description "Lacij is a graph visualization library for Clojure\nwith automatic layouts, edition and undo/redo support.")
